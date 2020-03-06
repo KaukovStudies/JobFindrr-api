f831061be66568974778979e7ffab15a3ad9c8e4
Peter Kaukov <peter@kaukov.dev>
2020-03-06 22:19:35 +0200

    Add base api route

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World!\n')
+const express = require('express')
+
+const app = express()
+
+app.use((_req, res) => {
+	res.send({
+		route: '/',
+		message: 'JobFindrr API',
+		version: '0.0.1-alpha',
+	})
+})
+
+app.listen(process.env.PORT || 3000)
