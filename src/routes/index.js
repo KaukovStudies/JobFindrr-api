39d1b74b2ed051818b99b307c7e86bde502a14bc
Peter Kaukov <peter@kaukov.dev>
2020-03-08 01:36:18 +0200

    Add basic routes for each main model

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 
+const companies = require('./companies')
+const countries = require('./countries')
+const users = require('./users')
+const skills = require('./skills')
 const main = require('./main')
+
+router.use('/companies', companies)
+router.use('/countries', countries)
+router.use('/users', users)
+router.use('/skills', skills)
 
 // Main routes
 router.use(main)
