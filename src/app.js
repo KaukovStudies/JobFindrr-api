ddf2a23c52f7fed44fd41becd45a9dac533608cc
Peter Kaukov <peter@kaukov.dev>
2020-03-07 03:09:48 +0200

    Add serving of static files. Add cat test image
    Image provided royalty-free by Pexels: https://www.pexels.com/photo/photo-of-tabby-cat-3777622/

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { json: bpJson, urlencoded: bpUrlEncoded, raw: bpRaw } = require('body-parser')
 
 const routes = require('./routes')
+const { getFilePath } = require('./utils')
 
 const port = process.env.PORT || 3000
 
@@ -10,6 +11,9 @@ const app = express()
 app.use(bpJson())
 app.use(bpUrlEncoded({ extended: false }))
 app.use(bpRaw())
+
+// Serve static files from a specific directory
+app.use(express.static(getFilePath('static')))
 
 app.use(routes)
 
