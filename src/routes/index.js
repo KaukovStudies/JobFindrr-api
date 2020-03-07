76daeae76cf1d1228d12ad1e57d2fed5dd6ea5d8
Peter Kaukov <peter@kaukov.dev>
2020-03-08 01:40:10 +0200

    Shorten routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,9 @@
 const router = require('express').Router()
 
-const companies = require('./companies')
-const countries = require('./countries')
-const users = require('./users')
-const skills = require('./skills')
-const main = require('./main')
-
-router.use('/companies', companies)
-router.use('/countries', countries)
-router.use('/users', users)
-router.use('/skills', skills)
-
-// Main routes
-router.use(main)
+router.use('/companies', require('./companies'))
+router.use('/countries', require('./countries'))
+router.use('/users', require('./users'))
+router.use('/skills', require('./skills'))
+router.use(require('./main'))
 
 module.exports = router
