a2a42b7e2bd74c5e234ed44acad05a60b51730fb
Peter Kaukov <peter@kaukov.dev>
2020-03-07 19:21:04 +0200

    Basic express app setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('Hello World!\n')
+// .env configuration import
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const port = process.env.APP_PORT || 3000
+
+const app = express()
+
+// body-parser imports
+app.use(bodyParser.json())
+app.use(bodyParser.raw())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(port)
