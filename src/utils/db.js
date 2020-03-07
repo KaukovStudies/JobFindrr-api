9714d607d3030add4abee24d0dec66f82c9e49e2
Peter Kaukov <peter@kaukov.dev>
2020-03-07 19:38:44 +0200

    Add DB util

---
+++ src/utils/db.js
@@ -0,0 +1,14 @@
+const { Sequelize, DataTypes } = require('sequelize')
+
+const db = new Sequelize({
+	host: process.env.DB_HOST || 'localhost',
+	username: process.env.DB_USER || 'root',
+	password: process.env.DB_PASS || '',
+	database: process.env.DB_NAME || '',
+	dialect: process.env.DB_DRIVER || 'mysql'
+})
+
+module.exports = {
+	db,
+	dataTypes: DataTypes
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
 const { rootDir, getFilePath } = require('./paths')
+const { db, dataTypes } = require('./db')
 
 module.exports = {
 	rootDir,
-	getFilePath
+	getFilePath,
+	db,
+	dataTypes
 }
