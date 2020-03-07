48e6ceac8575c5976321fcce29949256ed7ef1df
Peter Kaukov <peter@kaukov.dev>
2020-03-07 19:34:34 +0200

    Add serving of static files. Add cat image.
    Image provided royalty-free by Pexels: https://www.pexels.com/photo/photo-of-tabby-cat-3777622/

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const { getFilePath } = require('./utils')
 const routes = require('./routes')
 
 const port = process.env.APP_PORT || 3000
@@ -14,6 +15,9 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.raw())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Static file serving
+app.use(express.static(getFilePath('static')))
 
 // Add all routes
 app.use(routes)
